9755ca53d4ee439bc263b149264ffbc6d48c9fb1
Gpat71 <63578389+Gpat71@users.noreply.github.com>
2024-09-09 09:11:28 +0200

    Add files via upload

---
+++ LoRaReceiver_Water_Level_Display.ino
@@ -0,0 +1,116 @@
+#include "heltec.h"
+#include "Arduino.h"
+#include "LoRaWan_APP.h"
+#include "images.h"
+#include "Wire.h"  
+#include "HT_SSD1306Wire.h"
+
+//OLED pins
+#define OLED_SDA 17
+#define OLED_SCL 18 
+#define OLED_RST 21
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+
+#define RF_FREQUENCY                                915000000 // Hz
+
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+static RadioEvents_t RadioEvents;
+
+int16_t txNumber;
+
+int16_t rssi,rxSize;
+
+bool lora_idle = true;
+
+SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+
+void logo(){
+	factory_display.clear();
+	factory_display.drawXbm(0,5,logo_width,logo_height,(const unsigned char *)logo_bits);
+	factory_display.display();
+}
+
+
+void setup() {
+    Serial.begin(115200);
+    Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+    
+	factory_display.init();
+	factory_display.clear();
+	factory_display.display();
+	logo();
+	delay(300);
+	factory_display.clear();
+
+
+    txNumber=0;
+    rssi=0;
+  
+    RadioEvents.RxDone = OnRxDone;
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                               LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                               LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                               0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+
+
+
+	
+
+
+}
+
+
+
+void loop()
+{
+  if(lora_idle)
+  {
+    lora_idle = false;
+    Serial.println("into RX mode");
+    Radio.Rx(0);
+  }
+  Radio.IrqProcess( );
+}
+
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
+{
+    rssi=rssi;
+    rxSize=size;
+    memcpy(rxpacket, payload, size );
+    rxpacket[size]='\0';
+    Radio.Sleep( );
+    Serial.printf("\r\nreceived packet \"%s\" with rssi %d , length %d\r\n",rxpacket,rssi,rxSize);
+    
+    factory_display.drawString(0, 10,"Water Level Receiver");
+    factory_display.drawString(0, 20,rxpacket);
+    factory_display.display();
+    delay(100);
+    factory_display.clear();
+    lora_idle = true;
+}
